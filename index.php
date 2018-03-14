3024d1fa9d19e7940ebf1841f9664699c1c96604
danieloslan <daniel.alwi.oslan@student.untan.ac.id>
2018-03-14 14:21:30 +0700

    latihan 1, halaman perdana

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+//ini adalah komentar
+//ini komentar yang sangat panjang
+//taukah kamu komentar ini sangat panjang ?
+//wow panjang sekali komentar ini !!
+
+	echo "Daniel Alwi<br>";
+	echo "H11011611038<br>";
+
+	echo "Hello World<br>";
+
+ ?>
